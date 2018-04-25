58080644a04a6c49bc7ea16b02e91491fff57ad9
Jhonnatan alexander Casas leon <JhonnatanCasasLeon@Jhonnatans-MacBook.local>
2018-04-25 16:43:14 -0500

    agrego columna price_per_night y campo en el formulario

--- app/models/room.rb
+++ app/models/room.rb
@@ -2,18 +2,19 @@
 #
 # Table name: rooms
 #
-#  id          :integer          not null, primary key
-#  title       :string
-#  description :text
-#  beds        :integer
-#  guests      :integer
-#  created_at  :datetime         not null
-#  updated_at  :datetime         not null
-#  image_url   :string
+#  id              :integer          not null, primary key
+#  title           :string
+#  description     :text
+#  beds            :integer
+#  guests          :integer
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  image_url       :string
+#  price_per_night :float
 #
 
 class Room < ActiveRecord::Base
-	validates :title, presence: true
+	validates :title, :price_per_night, presence: true
 	validates :description, presence: true, length: { maximum: 400 }
 	validates :beds, numericality: { only_integer: true }, presence: true
 	validates :guests, numericality: { only_integer: true }, presence: true
